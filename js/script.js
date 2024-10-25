5ab2e846debe9a13a9af320a02203819e9e46489
Umbugerald <jengkolwarteg03@gmail.com>
2024-10-25 16:47:02 +0700

    mengubah sesuatu

--- js/script.js
+++ js/script.js
@@ -5,11 +5,25 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (event) => {
+  searchForm.classList.toggle('active');
+  event.preventDefault();
+}
+
 // KLIK DILUAR SIDE BAR UNTUK MENGHILANGKAN NAV
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
